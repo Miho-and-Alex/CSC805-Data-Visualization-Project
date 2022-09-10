8baf16d442a280a8fd2a0924f0f3785e17203580
AlexHappyCode <arpenacoding@gmail.com>
2022-09-09 20:35:10 -0700

    rendering is working with app.engine

--- node/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-var port = 3000;
-app.get('/', function (req, res) { return res.send('Hello Multiverse!'); });
-app.listen(port, function () { return console.log("Example app listening on port " + port + "!"); });
--- node/index.ts
+++ node/index.ts
@@ -10,9 +10,8 @@ app.use(express.static("public"));
 console.log("hello sup")
 
 app.set('view engine', 'ejs')
-app.set("views", path.join(__dirname, "../views"));
-app.get('/', (req, res) => res.render('index.html'))
+app.engine('html', require('ejs').renderFile)
 
-//app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../views/index.html')))
+app.get('/', (req, res) => res.render('index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
