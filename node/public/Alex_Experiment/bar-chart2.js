d27d50b12714f2a7f0e5fc7b7b4c4189af9b8c67
AlexHappyCode <arpenacoding@gmail.com>
2022-10-08 18:05:31 -0700

    prettiered

--- node/public/Alex_Experiment/bar-chart2.js
+++ node/public/Alex_Experiment/bar-chart2.js
@@ -1,19 +1,19 @@
-import * as d3 from "https://cdn.skypack.dev/d3@7";
-import { selectAll } from "https://cdn.skypack.dev/d3-selection@3";
+import * as d3 from "https://cdn.skypack.dev/d3@7"
+import { selectAll } from "https://cdn.skypack.dev/d3-selection@3"
 
 async function getCsvData(path) {
-  let arrData = [];
+  let arrData = []
   await d3.csv(path, function (data) {
-    arrData.push(data);
-  });
-  return arrData;
+    arrData.push(data)
+  })
+  return arrData
 }
 
 function lineChart() {
   // set the dimensions and margins of the graph
   var margin = { top: 10, right: 30, bottom: 30, left: 60 },
     width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    height = 400 - margin.top - margin.bottom
 
   // append the svg object to the body of the page
   var svg = d3
@@ -22,27 +22,27 @@ function lineChart() {
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
   //Read the data
   d3.csv(
     "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_IC.csv",
     function (data) {
       // Add X axis --> it is a date format
-      var x = d3.scaleLinear().domain([1, 100]).range([0, width]);
+      var x = d3.scaleLinear().domain([1, 100]).range([0, width])
       svg
         .append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+        .call(d3.axisBottom(x))
     }
-  );
+  )
 }
 
 let run = async () => {
-  let data = await getCsvData("songs_normalize.csv");
-  console.log(data);
+  let data = await getCsvData("songs_normalize.csv")
+  console.log(data)
 
   //d3.select("body").append()
-};
+}
 
-run();
+run()
