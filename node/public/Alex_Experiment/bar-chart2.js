5b357635637cb7691589a907e33f9cc6b66d98cf
AlexHappyCode <arpenacoding@gmail.com>
2022-10-12 21:27:54 -0700

    learning d3

--- node/public/Alex_Experiment/bar-chart2.js
+++ node/public/Alex_Experiment/bar-chart2.js
@@ -1,10 +1,11 @@
-import * as d3 from "https://cdn.skypack.dev/d3@7"
-import { selectAll } from "https://cdn.skypack.dev/d3-selection@3"
+import * as d3 from 'https://cdn.skypack.dev/d3@7'
+import { selectAll } from 'https://cdn.skypack.dev/d3-selection@3'
 
 async function getCsvData(path) {
   let arrData = []
   await d3.csv(path, function (data) {
-    arrData.push(data)
+    //arrData.push(data)
+    arrData = data
   })
   return arrData
 }
@@ -17,30 +18,73 @@ function lineChart() {
 
   // append the svg object to the body of the page
   var svg = d3
-    .select("#my_dataviz")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .select('#my_dataviz')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
+  let data = getCsvData('./songs_normalize.csv')
   //Read the data
-  d3.csv(
-    "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_IC.csv",
-    function (data) {
-      // Add X axis --> it is a date format
-      var x = d3.scaleLinear().domain([1, 100]).range([0, width])
-      svg
-        .append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-    }
-  )
+  // d3.csv('songs_normalize.csv', function (data) {
+  // Add X axis --> it is a date format
+  var x = d3.scaleLinear().domain([2000, 2022]).range([0, width])
+  svg
+    .append('g')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(d3.axisBottom(x))
+
+  // Add Y axis
+  var y = d3.scaleLinear().domain([-100, 100]).range([height, 0])
+  svg.append('g').call(d3.axisLeft(y))
+
+  // Show confidence interval
+  svg
+    .append('path')
+    .datum(data)
+    .attr('fill', '#cce5df')
+    .attr('stroke', 'none')
+    .attr(
+      'd',
+      d3
+        .area()
+        .x(function (d) {
+          return x(d.year)
+        })
+        .y0(function (d) {
+          return y(d.CI_right)
+        })
+        .y1(function (d) {
+          return y(d.CI_left)
+        })
+    )
+
+  // Add the line
+  svg
+    .append('path')
+    .datum(data)
+    .attr('fill', 'none')
+    .attr('stroke', 'steelblue')
+    .attr('stroke-width', 1.5)
+    .attr(
+      'd',
+      d3
+        .line()
+        .x(function (d) {
+          return x(d.year)
+        })
+        .y(function (d) {
+          return y(d.popularity)
+        })
+    )
 }
 
 let run = async () => {
-  let data = await getCsvData("songs_normalize.csv")
+  let data = await getCsvData('./songs_normalize.csv')
   console.log(data)
+
+  lineChart()
 
   //d3.select("body").append()
 }
