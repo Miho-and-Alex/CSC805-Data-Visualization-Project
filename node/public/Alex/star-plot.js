26f2f5ad14a7bda96e12792f7e59e05a225a11c5
AlexHappyCode <arpenacoding@gmail.com>
2022-11-22 18:16:22 -0800

    need to clean data to remove columns that are strings because otherwise it breaks the star plot

--- node/public/Alex/star-plot.js
+++ node/public/Alex/star-plot.js
@@ -1,10 +1,11 @@
 import * as d3 from 'https://cdn.skypack.dev/d3@7'
 import { selectAll } from 'https://cdn.skypack.dev/d3-selection@3'
 
-function startPlot(data) {
-  const MARGIN = { LEFT: 250, RIGHT: 10, TOP: 150, BOTTOM: 70 }
-  const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
-  const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
+function starPlot(data) {
+  const MARGIN = { LEFT: 50, RIGHT: 50, TOP: 50, BOTTOM: 50 }
+  const WIDTH = 700 - MARGIN.LEFT - MARGIN.RIGHT
+  const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
+  const [cx, cy] = [300, 200]
 
   const svg = d3
     .select('#radar-chart-area')
@@ -19,7 +20,7 @@ function startPlot(data) {
   // Can append the radar chart below
 
   let res = []
-  let features = ['A', 'B', 'C', 'D', 'E', 'F']
+
   //generate the data
   for (var i = 0; i < 3; i++) {
     var point = {}
@@ -27,28 +28,112 @@ function startPlot(data) {
     data.columns.forEach(f => (point[f] = 1 + Math.random() * 8))
     res.push(point)
   }
-  console.log(res)
 
   let radialScale = d3.scaleLinear().domain([0, 10]).range([0, 150])
 
   let ticks = [2, 4, 6, 8, 10]
 
+  // adding rings
   ticks.forEach(t =>
     g
       .append('circle')
-      .attr('cx', 0)
-      .attr('cy', 0)
+      .attr('cx', cx)
+      .attr('cy', cy)
       .attr('fill', 'none')
       .attr('stroke', 'gray')
       .attr('r', radialScale(t))
   )
 
+  // labels for rings
   ticks.forEach(t =>
-    svg.append("text")
-    .attr("x", 100)
-    .attr("y", 200 - radialScale(t))
-    .text(t.toString())
-);
+    g
+      .append('text')
+      .attr('x', cx)
+      .attr('y', cy - radialScale(t))
+      .text(t.toString())
+  )
+
+  function angleToCoordinate2(angle, value, column) {
+    // need new scale for each column
+
+    let radialScale = d3
+      .scaleLinear()
+      .domain([d3.min(data, d => d[column]), d3.max(data, d => d[column])])
+      .range([0, 150])
+    let x = Math.cos(angle) * radialScale(value)
+    let y = Math.sin(angle) * radialScale(value)
+    return { x: cx + x, y: cy - y }
+  }
+
+  function angleToCoordinate(angle, value) {
+    // need new scale for each column
+
+    let x = Math.cos(angle) * radialScale(value)
+    let y = Math.sin(angle) * radialScale(value)
+    return { x: cx + x, y: cy - y }
+  }
+
+  for (let i = 0; i < data.columns.length; i++) {
+    let column = data.columns[i]
+
+    let angle = Math.PI / 2 + (2 * Math.PI * i) / data.columns.length
+    let line_coordinate = angleToCoordinate(angle, 10)
+    let label_coordinate = angleToCoordinate(angle, 12.5)
+
+    //draw axis line
+    g.append('line')
+      .attr('x1', cx)
+      .attr('y1', cy)
+      .attr('x2', line_coordinate.x)
+      .attr('y2', line_coordinate.y)
+      .attr('stroke', 'black')
+
+    //draw axis label
+    g.append('text')
+      .attr('x', label_coordinate.x - 25)
+      .attr('y', label_coordinate.y)
+      .text(column)
+  }
+
+  let line = d3
+    .line()
+    .x(d => d.x)
+    .y(d => d.y)
+  let colors = ['darkorange', 'gray', 'navy']
+
+  function getPathCoordinates(sample) {
+    let coordinates = []
+    for (var i = 0; i < data.columns.length; i++) {
+      let column = data.columns[i]
+      if (typeof sample[column] == 'string') {
+        coordinates.push({ x: cx, y: cy })
+        continue
+      }
+      //console.log(data.columns[i]);
+      //console.log('columns:', data_point[data.columns[i]]);
+      let angle = Math.PI / 2 + (2 * Math.PI * i) / data.columns.length
+      let coord = angleToCoordinate2(angle, sample[column], column)
+      console.log(column, sample[column], coord)
+      coordinates.push(coord)
+    }
+    return coordinates
+  }
+
+  for (var i = 0; i < 1; i++) {
+    let d = data[i]
+    let color = colors[i]
+    let coordinates = getPathCoordinates(d)
+
+    //draw the path element
+    g.append('path')
+      .datum(coordinates)
+      .attr('d', line)
+      .attr('stroke-width', 1)
+      .attr('stroke', 'red')
+      .attr('fill', color)
+      .attr('stroke-opacity', 1)
+      .attr('opacity', 0.5)
+  }
 }
 
 let main = async () => {
@@ -56,7 +141,7 @@ let main = async () => {
     ...data,
     year: +data.year,
     duration_ms: +data.duration_ms,
-    explicit: +data.explicit,
+    explicit: data.explicit ? 1 : 0,
     popularity: +data.popularity,
     danceability: +data.danceability,
     energy: +data.energy,
@@ -71,7 +156,7 @@ let main = async () => {
     tempo: +data.tempo,
   }))
 
-  startPlot(data, 'popularity')
+  starPlot(data, 'popularity')
 }
 
 main()
