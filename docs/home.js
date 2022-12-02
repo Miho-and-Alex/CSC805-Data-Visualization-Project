05fb1aa5ce8c44f407c8b284b07e22eb009eb339
AlexHappyCode <arpenacoding@gmail.com>
2022-12-01 16:23:00 -0800

    prettier formatting

--- docs/home.js
+++ docs/home.js
@@ -1,13 +1,13 @@
-import * as d3 from "https://cdn.skypack.dev/d3@7";
+import * as d3 from 'https://cdn.skypack.dev/d3@7'
 
-d3.select("#bar-chart-icon").on("click", () => {
-  document.getElementById("nav-barchart-tab").click()
-});
+d3.select('#bar-chart-icon').on('click', () => {
+  document.getElementById('nav-barchart-tab').click()
+})
 
-d3.select("#scatter-plot-icon").on("click", () => {
-  document.getElementById("nav-scatterplot-tab").click()
-});
+d3.select('#scatter-plot-icon').on('click', () => {
+  document.getElementById('nav-scatterplot-tab').click()
+})
 
-d3.select("#circle-packing-icon").on("click", () => {
-  document.getElementById("nav-circlepacking-tab").click()
-});
+d3.select('#circle-packing-icon').on('click', () => {
+  document.getElementById('nav-circlepacking-tab').click()
+})
