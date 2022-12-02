24bb583498a63533854cbe11b212e18db30bfc64
AlexHappyCode <arpenacoding@gmail.com>
2022-12-01 16:12:07 -0800

    added cool clickable icons

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,5 @@
+import './home.js'
 import "./circle-packing.js";
 import "./star-plot.js";
-import "./scatter-plot.js";
-import "./bar-chart.js";
+import "./scatter-plot.js"
+import "./bar-chart.js"
